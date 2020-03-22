c0d79fc70c48b196e47266d97ac9dba41f865bdd
Facundo Rotger <facu_rot@hotmail.com>
2020-03-21 21:48:48 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,39 +1,43 @@
 var x_vals = [];
 var y_vals = [];
 
-let a, b, c;
+var m, b;
 
-const learningRate = 0.2;
-const optimizer = tf.train.adam(learningRate);
+const learningRate = 0.5;
+const optimizer = tf.train.sgd(learningRate);
 
 function setup() {
   createCanvas(400, 400);
 
-  a = tf.scalar(random(-1, 1)).variable();
-  b = tf.scalar(random(-1, 1)).variable();
-  c = tf.scalar(random(-1, 1)).variable();
-}
-
-function predict(equis) {
-  const x_tensor = tf.tensor1d(equis);
-
-  const y_tensor = a
-    .mul(x_tensor.square())
-    .add(b.mul(x_tensor))
-    .add(c);
-  return y_tensor;
+  // Inicializo m y b con valores random entre 0 y 1
+  // Estos son los valores que van a tener que ser optimizados
+  m = tf.variable(tf.scalar(random(1)));
+  b = tf.variable(tf.scalar(random(1)).variable());
 }
 
 function loss(pred, label) {
+  // pred son las predicciones
+  // label son las y de cada x
+
+  // Mean Square Root
   return pred
     .sub(label)
     .square()
     .mean();
 }
 
+function predict(equis) {
+  // Transformo las x a tensores
+  const x_tensor = tf.tensor1d(equis);
+
+  const y_tensor = x_tensor.mul(m).add(b);
+
+  return y_tensor;
+}
+
 function mousePressed() {
-  var x = map(mouseX, 0, width, -1, 1);
-  var y = map(mouseY, 0, height, 1, -1);
+  var x = map(mouseX, 0, width, 0, 1);
+  var y = map(mouseY, 0, height, 1, 0);
 
   x_vals.push(x);
   y_vals.push(y);
@@ -42,8 +46,8 @@ function mousePressed() {
 function draw() {
   tf.tidy(() => {
     if (x_vals.length > 0) {
-      const y_labels = tf.tensor1d(y_vals);
-      optimizer.minimize(() => loss(predict(x_vals), y_labels));
+      const y_tensor = tf.tensor1d(y_vals);
+      optimizer.minimize(() => loss(predict(x_vals), y_tensor));
     }
   });
 
@@ -51,28 +55,25 @@ function draw() {
   stroke(255);
 
   for (var i = 0; i < x_vals.length; i++) {
-    const pointX = map(x_vals[i], -1, 1, 0, width);
-    const pointY = map(y_vals[i], -1, 1, height, 0);
+    const pointX = map(x_vals[i], 0, 1, 0, width);
+    const pointY = map(y_vals[i], 0, 1, height, 0);
     strokeWeight(8);
     point(pointX, pointY);
   }
 
-  var lineX = [];
-  for (var i = -1; i < 1.01; i += 0.05) {
-    lineX.push(i);
-  }
-  var y = tf.tidy(() => predict(lineX));
-  var lineY = y.dataSync();
-  y.dispose();
+  const xs = [0, 1];
 
-  beginShape();
-  noFill();
-  stroke(255);
+  const ys = tf.tidy(() => predict(xs));
+  let lineY = ys.dataSync();
+  ys.dispose();
+
+  const x1 = map(xs[0], 0, 1, 0, width);
+  const x2 = map(xs[1], 0, 1, 0, width);
+
+  const y1 = map(lineY[0], 0, 1, height, 0);
+  const y2 = map(lineY[1], 0, 1, height, 0);
+
   strokeWeight(2);
-  for (var i = 0; i < lineX.length; i++) {
-    var xpoint = map(lineX[i], -1, 1, 0, width);
-    var ypoint = map(lineY[i], -1, 1, height, 0);
-    vertex(xpoint, ypoint);
-  }
-  endShape();
+  line(x1, y1, x2, y2);
 }
+
